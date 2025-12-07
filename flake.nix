043fd889f439c4e3e432688857757181fd903589
Robert McCoy <robertmccoy1981@gmail.com>
2025-12-07 10:12:51 -0500

    Update NixOS configuration for Framework 13 laptop

--- flake.nix
+++ flake.nix
@@ -1,32 +1,83 @@
 {
-  description = "Configuration for NixOS for Framework laptop";
+  description = "Framework 13 (12th-gen Intel) NixOS configuration for robert";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    home-manager = {
-      url = "github:nix-community/home-manager";
+    # Main nixpkgs – everything defaults to unstable
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Secondary stable channel
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    _1password-shell-plugins.url = "github:1Password/shell-plugins";
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, home-manager, nixpkgs, ... } @inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-stable
+    , home-manager
+    , nixos-hardware
+    , stylix
+    , _1password-shell-plugins
+    , impermanence
+    , ...
+    }:
     let
       system = "x86_64-linux";
-      
-      pkgs = import nixpkgs {
+    in {
+      nixosConfigurations."framework-13" = nixpkgs.lib.nixosSystem {
         inherit system;
-        config = { allowUnfree = true; };
-      };
-    in
-    {
-      nixosConfigurations = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs; };
+
         modules = [
-          ./host.nix
-          ./home.nix
+          ./configuration.nix
+
+          # Hardware
+          nixos-hardware.nixosModules.framework-12th-gen-intel
+
+          # Theming & impermanence
+          stylix.nixosModules.stylix
+          impermanence.nixosModules.impermanence
+
+          # 1Password shell plugins
+          _1password-shell-plugins.nixosModules.default
+
+          # Overlay: expose stable as pkgs.stable
+          {
+            nixpkgs.overlays = [
+              (final: prev: {
+                stable = import nixpkgs-stable {
+                  inherit system;
+                  config = { allowUnfree = true; };
+                };
+              })
+            ];
+          }
+
+          # Home Manager as a NixOS module
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users."robert" = import ./home.nix;
+          }
         ];
-     });
-  };
+      };
+    };
 }
